c1fa18adf6ebb5155f48fd5bd12e13644f880aaf
NetMullet <lipper.corp@gmail.com>
2025-05-18 13:24:39 +0900

    Initial Commit

---
+++ Yomei/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Yomei
+//
+//  Created by Ryo Otsuka on 2025/05/18.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Yomei/YomeiApp.swift
@@ -0,0 +1,17 @@
+//
+//  YomeiApp.swift
+//  Yomei
+//
+//  Created by Ryo Otsuka on 2025/05/18.
+//
+
+import SwiftUI
+
+@main
+struct YomeiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
